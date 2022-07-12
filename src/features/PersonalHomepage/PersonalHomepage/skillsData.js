40da099c988563a561580415173658e5535a0c9c
Jakub Tober <jakubtober276@gmail.com>
2022-07-12 14:08:25 +0200

    Alin

---
+++ src/features/PersonalHomepage/PersonalHomepage/skillsData.js
@@ -0,0 +1,35 @@
+export const skills = [
+"Semantic & accesiable HTML",
+"Error handling",
+"Git",
+"Responsive Web Design",
+"React",
+"GitHub Pull Request & Review",
+//eslint-disable-next-line no-script-url
+"JavaScript: ES6+",
+"React Hooks",
+"NPM",
+"Working with API (fetch, axios)",
+"Redux (Toolkit)",
+"Immutability",
+"Promises, Async/Await",
+"Redeux-Saga",
+"Markdown",
+"Debugging",
+"React Router",
+"Scrum",
+"CSS Flexbox",
+"CSS Grid",
+"Trello",
+"Teamwork",
+"CSS BEM convencion",
+];
+
+export const nextSkills = [
+    "JS classes",
+    "Unit Testing",
+    "Node.js",
+    "TypeScript",
+    "Cypress",
+    "React Context",
+];
